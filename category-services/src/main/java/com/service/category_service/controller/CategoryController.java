d77d2521d96db489274cdea582973c20b8dc0936
Gokulakrishnan <gokulkrishna311201@gmail.com>
2025-12-06 13:40:42 +0530

    added category

---
+++ category-services/src/main/java/com/service/category_service/controller/CategoryController.java
@@ -0,0 +1,53 @@
+package com.service.category_service.controller;
+
+import com.service.category_service.dto.CategoryDto;
+import com.service.category_service.dto.CategoryReqDto;
+import com.service.category_service.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+
+    public String  port;
+    CategoryController(CategoryService categoryService){
+        this.categoryService=categoryService;
+    }
+    @Operation(description = "Get Category")
+    @GetMapping("/category")
+     public List<CategoryDto> getAllCategory(){
+      return   categoryService.getAllCategory();
+    }
+
+    @Operation(description = "Get Category by Id")
+    @GetMapping("/category/{id}")
+    public CategoryDto getCategoryById(@PathVariable() Long id){
+        return   categoryService.getCategoryById(id);
+    }
+
+    @Operation(description = "create  Category")
+    @PostMapping("/category")
+    public CategoryDto createCategory(@RequestBody() CategoryReqDto categoryDto){
+        return  categoryService.createCategory(categoryDto);
+    }
+
+    @Operation(description = "update  Category")
+    @PutMapping("/category/{id}")
+    public CategoryDto updateCategory(@PathVariable Long id,@RequestParam() CategoryDto categoryDto){
+
+        return  categoryService.updateCategory(id,categoryDto);
+    }
+    @Operation(description = "Get Category by name")
+    @GetMapping("/categoryByName")
+    public List<CategoryDto> getCategoryByName(@RequestParam() String name){
+        return   categoryService.findByName(name);
+    }
+
+}
---
+++ category-services/src/main/java/com/service/category_service/dto/CategoryDto.java
@@ -0,0 +1,6 @@
+package com.service.category_service.dto;
+
+public record CategoryDto(Long id, String name, String description) {
+}
+//record CategoryReqDto(Long id, String name, String description) {
+//}
